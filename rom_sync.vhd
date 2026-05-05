a1f2c250252ffdfdfc107a4f3d6632f6a3a5c5b6
MatitasGamexD <123197238+MatitasGamexD@users.noreply.github.com>
2026-05-04 20:57:52 -0500

    rom_sync.vhd

--- rom_sync.vhd
+++ rom_sync.vhd
@@ -1,49 +1,40 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
-library work;
-use work.mem_pkg.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity rom_sync is
-    port(
-        clk      : in  STD_LOGIC;
-        re       : in  STD_LOGIC;
-        addr     : in  addr_t;
-        data_out : out data_t
-    );
-end rom_sync;
+  generic (
+    DATA_WIDTH : positive := 8;
+    ADDR_WIDTH : positive := 4
+  );
+  port (
+    clk      : in  std_logic;
+    addr     : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
+    data_out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+  );
+end entity;
 
-architecture Behavioral of rom_sync is
+architecture rtl of rom_sync is
+  type rom_type is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
+  constant mem : rom_type := (
+    0 => x"AA", 1 => x"55", 2 => x"F0", 3 => x"0F",
+    4 => x"FF", 5 => x"00",
+    others => (others => '0')
+  );
+  signal q_reg : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal addr_i: integer range 0 to 2**ADDR_WIDTH-1;
 
-    constant ROM_CONTENT : mem_t := (
-        0  => x"12",
-        1  => x"34",
-        2  => x"56",
-        3  => x"78",
-        4  => x"9A",
-        5  => x"BC",
-        6  => x"DE",
-        7  => x"F0",
-        8  => x"0F",
-        9  => x"1E",
-        10 => x"2D",
-        11 => x"3C",
-        12 => x"4B",
-        13 => x"5A",
-        14 => x"69",
-        15 => x"FF"
-    );
-
+  attribute romstyle : string;
+  attribute romstyle of mem : constant is "M9K";
 begin
+  addr_i <= to_integer(unsigned(addr));
 
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if re = '1' then
-                data_out <= ROM_CONTENT(to_integer(unsigned(addr)));
-            end if;
-        end if;
-    end process;
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      q_reg <= mem(addr_i);
+    end if;
+  end process;
 
-end Behavioral;
+  data_out <= q_reg;
+end architecture;
